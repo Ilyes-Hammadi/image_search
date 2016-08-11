bde2253ec8724330be6bd8d021378a8013a211a0
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2016-08-12 00:53:13 +0100

    feat: Add the pagination

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -13,6 +13,21 @@ var cx = '012824588551273422190:_vqtcpfysom'
 var key = 'AIzaSyD7snD0Pp0v1sadfYoh18WPddamXuMnKwU'
 var originalSearchUrl = 'https://www.googleapis.com/customsearch/v1?searchType=image&cx=' + cx + '&key=' + key + '&q=';
 
+
+function isNormalInteger(str) {
+    var n = ~~Number(str);
+    return String(n) === str && n >= 0;
+}
+
+function constructUrl(query, offset) {
+  if(offset){
+    return originalSearchUrl + query + "&start=" + (Number(offset) * 10);
+  }else {
+    return originalSearchUrl + query;
+  }
+}
+
+
 router.get('/', function (req, res, next) {
     res.render('index');
 });
@@ -21,23 +36,31 @@ router.get('/', function (req, res, next) {
 router.get('/api/imagesearch/', (req, res) => {
 
   var query = req.param('q');
+  var offset = req.param('offset')
   var res_data = [];
 
   // If the query is not empty
   if(/\S/.test(query)) {
-    // Create the search url
-    searchUrl = originalSearchUrl + query;
 
-    // Create Date object
-    var date = new Date();
+    // If the offset pram is passed
+    if(isNormalInteger(offset)) {
+
+      // Create the search url with the offset param
+      searchUrl = constructUrl(query, offset);
+    }else {
+      // Create the search url
+      searchUrl = constructUrl(query);
+    }
 
     // save the user query with the acctual
-    var search = new Search({term : query, when : date.toJSON()})
+    var search = new Search({term : query, when : Date.now()})
 
     // Create the new url entry in the database
     search.save((err) => {
       if (err) console.log(err);
     });
+
+    console.log(searchUrl);
 
     // Make the request and create the json response
     request(searchUrl, (error, response, body) => {
@@ -52,8 +75,11 @@ router.get('/api/imagesearch/', (req, res) => {
               })
             }
             return res.json(res_data);
+        }else {
+          res.json(error)
         }
     })
+
 
   } else {
     return res.json({
